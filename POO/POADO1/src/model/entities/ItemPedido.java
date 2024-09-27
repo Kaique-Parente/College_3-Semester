7f6ec3fd14dba910389a441825a544d50758b82f
Kaique Parente <kaique.parente.game@outlook.com>
2024-09-27 01:51:05 -0300

    Update Finished

--- POO/POADO1/src/model/entities/ItemPedido.java
+++ POO/POADO1/src/model/entities/ItemPedido.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.entities;
 
-/**
- *
- * @author kaiqu
- */
 public class ItemPedido {
     
     private Integer quantidade;
